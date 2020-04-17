f6039cbf54db7854db33fa670f5eb3ab8d1d0c92
TheMulti0 <multi@codeprecise.com>
2020-04-17 09:55:10 +0300

    Bootstrap

--- src/navigation/Component.tsx
+++ src/navigation/Component.tsx
@@ -1,16 +1,11 @@
-import './Component.css';
 import React from "react";
 import { IRouteMappingProps } from "../models/IRouteMappingProps";
 
 export default function Component(props: IRouteMappingProps) {
   const { mapping } = props;
   return (
-    <div className="container">
-
-      <mapping.component
-        mappings={mapping.mappings}
-        {...props} />
-
-    </div>
+    <mapping.component
+      mappings={mapping.mappings}
+      {...props} />
   );
 }
