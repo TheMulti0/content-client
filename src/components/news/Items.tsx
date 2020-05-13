c02497347d22960fefcca17497dbed1c0d9570d6
TheMulti0 <multi@codeprecise.com>
2020-05-13 11:43:32 +0300

    NewsItems uses Grid

--- src/components/news/Items.tsx
+++ src/components/news/Items.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, List, ListItem } from "@material-ui/core";
+import { Box, Divider, Grid, } from "@material-ui/core";
 import React from "react";
 import { INewsItem } from "../../models/INewsItem";
 import Item from "./Item";
@@ -7,11 +7,11 @@ export default function Items(props: { items: INewsItem[] }) {
   const { items } = props;
 
   return (
-    <List className="news">
+    <Grid container direction="column" spacing={ 5 } className="news">
       {
         items.map(NewsItem)
       }
-    </List>
+    </Grid>
   );
 }
 
@@ -19,9 +19,7 @@ function NewsItem(item: INewsItem, index: number) {
   return (
     <Box key={ index }>
 
-      <ListItem>
-        <Item item={ item }/>
-      </ListItem>
+      <Item item={ item }/>
 
       <br/>
       <Divider/>
